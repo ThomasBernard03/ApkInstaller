a6850a2da72c82daa087d95088019c861b97f9d7
Thomas Bernard <thomas.bernard@ikmail.com>
2025-02-02 17:36:02 +0100

    Init project ⚙️

---
+++ ApkInstaller/ApkInstallerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApkInstallerApp.swift
+//  ApkInstaller
+//
+//  Created by Thomas Bernard on 02/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct ApkInstallerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
